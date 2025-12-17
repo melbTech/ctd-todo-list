75f1cdf5b218db0f298d978e62d0a529bb9d5a31
Melvin Santos <103694007+melbTech@users.noreply.github.com>
2025-12-16 21:47:04 -0500

    Update states to allow user to add todos

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,22 @@ import TodoForm from './TodoForm.jsx';
 import { useState } from 'react';
 
 function App() {
-  const [newTodo, setNewTodo] = useState('Example Text');
+  const [todoList, setTodoList] = useState([]);
+
+  function addTodo(title) {
+    const newTodo = {
+      title: title,
+      id: Date.now(),
+    };
+
+    setTodoList([...todoList, newTodo]);
+  }
+
   return (
     <div>
       <h1>My Todos</h1>
-      <TodoForm></TodoForm>
-      <p>{newTodo}</p>
-      <TodoList></TodoList>
+      <TodoForm onAddTodo={addTodo}></TodoForm>
+      <TodoList todoList={todoList}></TodoList>
     </div>
   );
 }
