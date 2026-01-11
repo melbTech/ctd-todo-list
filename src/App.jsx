e8bda5b1df48ea19fe777cb39c38652e43f03118
Melvin Santos <103694007+melbTech@users.noreply.github.com>
2026-01-11 17:13:10 -0500

    Update TodoList to render message & allow user to complete todo

--- src/App.jsx
+++ src/App.jsx
@@ -10,16 +10,28 @@ function App() {
     const newTodo = {
       title: title,
       id: Date.now(),
+      isCompleted: false,
     };
 
     setTodoList([...todoList, newTodo]);
+  }
+
+  function completeTodo(id) {
+    const updatedTodos = todoList.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, isCompleted: true };
+      }
+      return todo;
+    });
+
+    setTodoList(updatedTodos);
   }
 
   return (
     <div>
       <h1>My Todos</h1>
       <TodoForm onAddTodo={addTodo}></TodoForm>
-      <TodoList todoList={todoList}></TodoList>
+      <TodoList todoList={todoList} onCompleteTodo={completeTodo}></TodoList>
     </div>
   );
 }
