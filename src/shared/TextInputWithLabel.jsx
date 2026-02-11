275703d3ce7432556ba4591753ac71b9a378771f
Melvin Santos <103694007+melbTech@users.noreply.github.com>
2026-02-11 10:41:21 -0500

    Add styles to the todo web app

--- src/shared/TextInputWithLabel.jsx
+++ src/shared/TextInputWithLabel.jsx
@@ -1,12 +1,21 @@
+import styled from 'styled-components';
+
+const StyledInput = styled.input`
+  font-size: 16px;
+  padding: 8px;
+  width: 80%;
+`;
+
 function TextInputWithLabel({ elementId, label, onChange, ref, value }) {
   return (
     <>
-      <label htmlFor={elementId}>{label}</label>
-      <input
+      <label htmlFor={elementId}></label>
+      <StyledInput
         type="text"
         id={elementId}
         ref={ref}
         value={value}
+        placeholder="Enter todo.."
         onChange={onChange}
       />
     </>
